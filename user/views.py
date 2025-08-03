3cc46f46132b4affb6b45c3a613a5e7af851acdd
Kyaw Pyae Sone <kyawpyaeson@ucsy.edu.mm>
2025-08-03 16:03:05 +0630

    User App

--- user/views.py
+++ user/views.py
@@ -1,17 +1,6 @@
 from django.shortcuts import render
 from django.views import View
 
-# Create your views here.
-
-# def render_login(request):
-#     return render(request, "user/login.html")
-#
-# def render_register(request):
-#     return render(request, "user/registration.html")
-#
-#
-# def render_profile(request):
-#     return render(request, "user/profile.html")
 
 class Login(View):
     def get(self, request):
