0c91ba7bb991a77e1cf83c688c1ce0480c1c0841
Kyaw Pyae Sone <kyawpyaeson@ucsy.edu.mm>
2025-08-25 05:41:11 +0630

    25/08/2025 05:41

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Order, OrderDetail
 
 # Register your models here.
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ("order_id", "user_name", "order_status", "payment")
+    list_filter = ("order_status","payment")
+
+@admin.register(OrderDetail)
+class OrderDetailAdmin(admin.ModelAdmin):
+    list_display = ("order_id", "phone_model", "quantity", "price")
--- orders/models.py
+++ orders/models.py
@@ -1,4 +1,44 @@
 from django.db import models
+from django.contrib.auth.models import User
+from dashboard.models import Phone
 
 # Create your models here.
 
+class Order(models.Model):
+    order_id = models.AutoField(primary_key=True)
+    user_name = models.ForeignKey(User, on_delete=models.CASCADE)
+    order_status = models.CharField(
+        max_length=100,
+        choices = [
+            ("cancel", "Cancel"),
+            ("pending", "Pending"),
+            ("confirm", "Confirm")
+        ]
+    )
+    grand_total = models.FloatField()
+    transaction_id = models.IntegerField
+    address = models.TextField
+    payment = models.CharField(
+        max_length=100,
+        choices=[
+            ("kbz", "KBZ Pay"),
+            ("cb", "CB Pay"),
+            ("wave", "Wave Pay"),
+            ("aya", "Aya Pay")          
+        ],
+        default="kbz"
+    )
+    order_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.order_id)
+
+
+class OrderDetail(models.Model):
+    order_id = models.ForeignKey(Order, on_delete=models.CASCADE)
+    phone_model = models.ForeignKey(Phone, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    price = models.IntegerField()
+
+    def __str__(self):
+        return str(self.order_id)
